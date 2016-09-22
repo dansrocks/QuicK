f677aa3f801171d0f37b7f849081d03cc32db7b2
Dans Norvey <drocks2000@gmail.com>
2016-09-23 01:14:58 +0200

    Adding basictemplate support. First DI (Dependency Injection) : container is now inject into controller.

--- public/index.php
+++ public/index.php
@@ -3,7 +3,14 @@
 require './vendor/autoload.php';
 
 
-$app = new \Slim\App;
+$container = new \Slim\Container();
+$container['view'] = function ($container) {
+    return new \Slim\Views\PhpRenderer('./templates/');
+};
+
+
+$app = new \Slim\App($container);
+
 $app->get('/', '\App\Controllers\Welcome:index');
 $app->get('/hello', '\App\Controllers\Welcome:hello');
 $app->get('/hello/{name}', '\App\Controllers\Welcome:hello');
--- src/Controllers/Welcome.php
+++ src/Controllers/Welcome.php
@@ -4,23 +4,57 @@ namespace App\Controllers;
 
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
+use Slim\Container;
 
 
 class Welcome
 {
-    public function index(Request $request, Response $response)
+    private $container = null;
+
+    /**
+     * @param Container $container
+     */
+    public function __construct($container)
     {
-            return $response->getBody()->write("Hola " . __CLASS__);
+        $this->container = $container;
     }
 
+    /**
+     * @param Request $request
+     * @param Response $response
+     * @return int
+     */
+    public function index(Request $request, Response $response)
+    {
+        return $response->getBody()->write("Hola " . __CLASS__);
+    }
+
+    /**
+     * @param Request $request
+     * @param Response $response
+     * @return Response
+     */
     public function hello(Request $request, Response $response)
     {
         $name = $request->getAttribute('name', null);
         if (! $name) {
             $name = 'unknown';
         }
+
+        $response = $this->View($response, 'home.phtml', ["tickets" => 1, "router" => 1]);
         $response->getBody()->write("Hello, $name");
 
         return $response;
+    }
+
+    /**
+     * @param Response  $response
+     * @param string    $tmpl
+     * @param mixed[]   $params
+     * @return mixed
+     */
+    private function View(Response $response, $tmpl, $params)
+    {
+        return $this->container['view']->render($response, $tmpl, $params);
     }
 }
